2e123761c0011edaf8def88ba2ca788976121a32
benwilliam <benwilliam@freenet.de>
2014-08-15 13:39:14 +0200

    Update animationDriver.cpp

--- WS2812b_SPI/ws2811_2/graphics/animationDriver.cpp
+++ WS2812b_SPI/ws2811_2/graphics/animationDriver.cpp
@@ -1,1 +1,36 @@
 
+void IAnimationDriver::updateAnimations(void){
+  while(performSteps > 0){
+    for(uint8_t i = 0; i<MAX_ANIMATION_COUNT; i++){
+      if(animations[i] !0 nullptr){
+          animation->perofrmAnimationStep();
+        }
+      }
+    }
+    performSteps--;
+}
+
+bool IAnimationDriver::registerAnimation(IAnimation &animation){
+  for(uint8_t i = 0; i<MAX_ANIMATION_COUNT; i++){
+    if(animations[i] == nullptr){
+        animations[i] = &animation;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void IAnimationDriver::deRegisterAnimation(IAnimation &animation){
+  for(uint8_t i = 0; i<MAX_ANIMATION_COUNT; i++){
+    if(animations[i] == &animation){
+        animations[i] = nullptr;
+        break;
+      }
+    }
+  }
+}
+
+void IAnimationDriver::handleAnimationISR(void){
+  performSteps++;
+}
