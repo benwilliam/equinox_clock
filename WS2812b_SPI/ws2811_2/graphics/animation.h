4acbbb3a29d38267a5d153997de4d7db0b37b2a7
benwilliam <benwilliam@freenet.de>
2014-08-15 13:45:14 +0200

    Update animation.h

--- WS2812b_SPI/ws2811_2/graphics/animation.h
+++ WS2812b_SPI/ws2811_2/graphics/animation.h
@@ -1,4 +1,15 @@
 
 class IAnimation{
-
+  public:
+    /** \brief perform one step of Animation
+     *
+     *  will be called periodically
+     */
+    void perofrmAnimationStep(void);
+    
+  protected:
+    /** \brief implement here your animation steps
+     *
+     */
+    virtual void doAnimationStep(void) = 0;
 };
